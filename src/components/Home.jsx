efc35b13bfbf4bdcaeaf4db86cb401cda62615b5
Aminul667 <rahathssn@gmail.com>
2024-08-19 16:55:43 +0600

    added cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,24 @@
+import { FaArrowRight } from "react-icons/fa";
+
 const Home = () => {
   return (
-    <div className="p-6 max-w-sm bg-background rounded-3xl shadow-neo text-center mt-4">
-      <h1 className="text-4xl font-bold text-text mb-4">Metropulse</h1>
-      <p className="text-2xl text-highlight">5/4 Time Signature</p>
-      <div className="mt-6">
-        <div className="p-4 bg-background rounded-full shadow-neo mb-4">
-          <span className="text-6xl text-text">75</span>
+    // <div className="p-2 max-w-sm bg-background rounded-3xl shadow-neo text-center mt-4 w-full">
+    //   <p className="text-2xl text-highlight">Active Medication</p>
+    //   <div className="mt-6">
+    //     <div className="p-4 bg-background rounded-full shadow-neo mb-4">
+    //       <span className="text-6xl text-text">75</span>
+    //     </div>
+    //     <button className="bg-highlight text-background py-2 px-6 rounded-full shadow-neo hover:bg-light">
+    //       Confirm
+    //     </button>
+    //   </div>
+    // </div>
+    <div className="my-4">
+      <h2 className="text-xl text-highlight">Active Medication</h2>
+      <div>
+        <div className="bg-highlight p-2 rounded-full shadow-lg h-4 w-4">
+          {/* <FaArrowRight size={12} className="text-background" /> */}
         </div>
-        <button className="bg-highlight text-background py-2 px-6 rounded-full shadow-neo hover:bg-light">
-          Confirm
-        </button>
       </div>
     </div>
   );

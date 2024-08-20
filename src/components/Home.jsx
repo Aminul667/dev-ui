6cd85bb80bf2cf1eb0fadda30ef7a063f8c11032
Aminul667 <rahathssn@gmail.com>
2024-08-20 14:16:29 +0600

    added active medication and incident

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,11 +13,56 @@ const Home = () => {
     //     </button>
     //   </div>
     // </div>
-    <div className="my-4">
-      <h2 className="text-xl text-highlight">Active Medication</h2>
-      <div>
-        <div className="bg-highlight p-2 rounded-full shadow-lg h-4 w-4">
-          {/* <FaArrowRight size={12} className="text-background" /> */}
+    // p-2 rounded-full shadow-lg h-2 w-2
+    <div>
+      {/* active medication */}
+      <div className="my-4">
+        <h2 className="text-xl text-highlight text-center">
+          Active Medication
+        </h2>
+        <div className="w-80">
+          <div className="border-l-2 border-highlight pl-2 text-text mt-3">
+            <p className="text-base">Maxpro</p>
+            <p className="text-sm">1 Morning, 1 Dinner</p>
+          </div>
+          <div className="border-l-2 border-highlight pl-2 text-text mt-3">
+            <p className="text-base">Fimoxil</p>
+            <p className="text-sm">1 Morning</p>
+          </div>
+          <div className="border-l-2 border-highlight pl-2 text-text mt-3">
+            <p className="text-base">Napa</p>
+            <p className="text-sm">2 Morning</p>
+          </div>
+        </div>
+      </div>
+
+      {/* recent incident */}
+      <div className="my-4">
+        <h2 className="text-xl text-highlight text-center">Recent Incident</h2>
+        <div className="w-80">
+          <div className="border-l-2 border-highlight pl-3 text-text mt-3">
+            <p className="text-base">4 Aug 2024</p>
+            <div className="pl-2">
+              <p className="text-sm">Headache (Mild)</p>
+              <p className="text-sm">Chest Pain (Moderate)</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-80">
+          <div className="border-l-2 border-highlight pl-3 text-text mt-3">
+            <p className="text-base">6 Aug 2024</p>
+            <div className="pl-2">
+              <p className="text-sm">Cold (Mild)</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-80">
+          <div className="border-l-2 border-highlight pl-3 text-text mt-3">
+            <p className="text-base">4 Aug 2024</p>
+            <div className="pl-2">
+              <p className="text-sm">Back Pain (Mild)</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
